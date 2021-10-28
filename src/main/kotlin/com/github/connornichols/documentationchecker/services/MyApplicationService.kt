aa688af1ec4aac9494e43cad9ee4068f6e2c23ec
GitHub Action <action@github.com>
2021-10-28 21:05:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/connornichols/documentationchecker/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.connornichols.documentationchecker.services
+
+import com.github.connornichols.documentationchecker.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
